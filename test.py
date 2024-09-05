06328203bc712ec8c0e70c7e5068998251b558e7
Sreeni <kattubadi426@gmail.com>
2024-09-05 09:11:14 +0530

    Read library

---
+++ test.py
@@ -0,0 +1,21 @@
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
+import json
+spark = SparkSession.builder.getOrCreate()
+
+#Read the JSON configuration file
+with open(r"C:\Users\A4952\PycharmProjects\test_automation_project\schema_files\contact_info_schema.json", 'r') as schema_file:
+        schema = StructType.fromJson(json.load(schema_file))
+
+print(schema)
+
+df = spark.read.schema(schema).csv(r"C:\Users\A4952\PycharmProjects\test_automation_project\files\Contact_info.csv", header=True )
+
+#df = spark.read.csv(r"C:\Users\A4952\PycharmProjects\test_automation_project\files\Contact_info.csv", header=True, inferSchema=True)
+
+df.printSchema()
+
+print(df.schema.json())
+
+df.show(truncate=False)
