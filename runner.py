de7e15a2bcec88a5b5972f42844366a28790ce75
Sreeni <kattubadi426@gmail.com>
2024-09-12 08:54:41 +0530

    Read library

--- runner.py
+++ runner.py
@@ -8,10 +8,9 @@ from utility.read_lib import read_file, read_snowflake, read_db
 # Jars setup
 project_path = os.getcwd()
 print("project_path", project_path)
-snow_jar = project_path+"\jar/snowflake-jdbc-3.14.3.jar"
-#snow_jar = r"C:\Users\A4952\PycharmProjects\June_automation_batch1\jars\snowflake-jdbc-3.14.3.jar"
+snow_jar = project_path+"\jar\snowflake-jdbc-3.14.3.jar"
 postgres_jar = project_path+"\jar\postgresql-42.2.5.jar"
-# postgres_jar = r"C:\Users\A4952\PycharmProjects\test_automation_project\jar\postgresql-42.2.5.jar"
+
 
 jars = snow_jar + ',' + postgres_jar
 
@@ -24,8 +23,9 @@ spark = SparkSession.builder.master("local[1]") \
     .config("spark.jars.packages", "org.apache.spark:spark-avro_2.12:3.4.0") \
     .getOrCreate()
 
+test_case_file_path = project_path + '\config\Master_Test_Template.xlsx'
 # Test cases read
-test_cases = pd.read_excel(r"C:\Users\A4952\PycharmProjects\test_automation_project\config\Master_Test_Template.xlsx")
+test_cases = pd.read_excel(test_case_file_path)
 
 run_test_case = test_cases.loc[(test_cases.execution_ind == 'Y')]
 print("Ind==Y records")
